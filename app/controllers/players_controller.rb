a468bd39f5372e854056cb5ea003f3082e0f6552
Thomas Dimperio <twdimperio@gmail.com>
2022-12-15 10:30:28 -0700

    Add index and show routes for Player

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,11 @@
 class PlayersController < ApplicationController
+
+  def index
+    render json: Player.all
+  end
+
+  def show
+    player = Player.find_by(name: params[:name])
+    render json: player
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :teams
   resources :users, only: [:create, :show]
   resources :nhl_teams, only: [:index, :show]
+  resources :players, only: [:index, :show]
 
   post '/login', to: 'sessions#create'
   get '/auth', to: 'users#show'
