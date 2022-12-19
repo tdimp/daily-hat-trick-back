38198a3841f4a2dcb75e5bac0e4624edd3c436f3
Thomas Dimperio <twdimperio@gmail.com>
2022-12-19 15:18:51 -0700

    Fix bugs in SessionsController and UsersController related to login functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,14 @@
 class SessionsController < ApplicationController
+  skip_before_action :authorize, only: [:create]
 
   def create
-    @user = user.find_by(username: params[:username])
+    user = User.find_by(email: params[:email])
     if user&.authenticate(params[:password])
-      session[:user_id] = @user.id
+      session[:user_id] = user.id
       render json: user, status: :created
     else 
       render json: { error: {login: "Invalid username or password"} }, status: :unauthorized
+    end
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   skip_before_action :authorize, only: [:show, :create]
 
   def show
-    @user = User.find_by(id: session[:user_id])
+    user = User.find_by(id: session[:user_id])
     if user
       render json: user
     else
