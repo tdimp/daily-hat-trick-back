c0b42af033887b572ee76ecf1a5b68c98be41591
Thomas Dimperio <twdimperio@gmail.com>
2022-12-26 16:20:47 -0700

    Add dependent: :destroy statements to User and Team models

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,6 @@
 class Team < ApplicationRecord
   belongs_to :user
 
-  has_many :team_players
+  has_many :team_players, dependent: :delete_all
   has_many :players, through: :team_players
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
   
-  has_many :teams
+  has_many :teams, dependent: :destroy
 end
