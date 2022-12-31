ef135cca88acbf460138ca9f3dcab6c54d9d874d
Thomas Dimperio <twdimperio@gmail.com>
2022-12-31 09:11:46 -0700

    Add stats associations to Player model and PlayerSerializer

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,16 +3,7 @@ class Player < ApplicationRecord
 
   has_many :team_players
   has_many :teams, through: :team_players
-  has_many :goalie_stats
   has_many :skater_stats
-
-  def self.stats
-    if self.position != "G"
-      return serialize :self.skater_stats
-    elsif self.position == "G"
-      return self.goalie_stats
-    else
-      return "Player has no stats"
-    end
-  end
+  has_many :goalie_stats
+  
 end
--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -2,6 +2,9 @@ class PlayerSerializer < ActiveModel::Serializer
   attributes :id, :name, :position, :jersey_number
   
   belongs_to :nhl_team, serializer: NhlTeamSerializer
+
+  has_many :goalie_stats
+  has_many :skater_stats
   
  
 end
