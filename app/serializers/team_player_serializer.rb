67663a859831fe407bbec690fb173503f366b150
Thomas Dimperio <twdimperio@gmail.com>
2023-02-03 08:06:33 -0700

    Add jersey_number to TeamPlayerSerializer

--- app/serializers/team_player_serializer.rb
+++ app/serializers/team_player_serializer.rb
@@ -1,3 +1,3 @@
 class TeamPlayerSerializer < ActiveModel::Serializer
-  attributes :id, :full_name, :position, :goalie_stat, :skater_stat
+  attributes :id, :full_name, :jersey_number, :position, :goalie_stat, :skater_stat
 end
